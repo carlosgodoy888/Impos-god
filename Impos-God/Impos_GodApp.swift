6af7470b00ec2530e16c91c15911caf7eada35f5
carlosgodoy888 <carlosgodoyvalverde@gmail.com>
2026-03-26 14:09:12 +0100

    Initial Commit

---
+++ Impos-God/Impos_GodApp.swift
@@ -0,0 +1,17 @@
+//
+//  Impos_GodApp.swift
+//  Impos-God
+//
+//  Created by Carlos Godoy Valverde on 26/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Impos_GodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
